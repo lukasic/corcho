9c8ebe0bd8ea5b7f2ba9aac85dbeeca560c342c6
Lukáš Kasič <lukas@kasic.sk>
2016-03-20 18:50:39 +0100

    Add models for courses

--- app/courses/admin.py
+++ app/courses/admin.py
@@ -1,3 +1,51 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from django import forms
+from import_export.admin import ImportExportActionModelAdmin
+
+from app.courses.models import SubjectClass, CourseCategory, Course
+
+
+class CourseCategoryAdminForm(forms.ModelForm):
+    """
+    Be caferul! Relation Course.category -> couse_set is just ForeignKey, not ManyToMany.
+    """
+    course_set = forms.ModelMultipleChoiceField(
+        Course.objects.all(),
+        widget=admin.widgets.FilteredSelectMultiple('course_set', False),
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(CourseCategoryAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.initial['course_set'] = self.instance.course_set.values_list('pk', flat=True)
+
+    def save(self, *args, **kwargs):
+        instance = super(CourseCategoryAdminForm, self).save(*args, **kwargs)
+        if instance.pk:
+            #instance.course_set.clear()
+            instance.course_set.add(*self.cleaned_data['course_set'])
+        return instance
+
+
+class SubjectClassAdmin(ImportExportActionModelAdmin):
+    list_display = ("acronym", "name")
+    filter_horizontal = ('teachers',)
+
+class CourseCategoryAdmin(admin.ModelAdmin):
+    list_display = ("acronym", "name", "description")
+    form = CourseCategoryAdminForm
+
+
+class CourseAdmin(ImportExportActionModelAdmin):
+    list_display = ("acronym", "name", "category", "subject_class")
+    list_filter = ("category", "subject_class")
+    list_editable = ("subject_class",)
+    filter_horizontal = ("teachers",)
+
+
+admin.site.register(SubjectClass, SubjectClassAdmin)
+admin.site.register(CourseCategory, CourseCategoryAdmin)
+admin.site.register(Course, CourseAdmin)
--- app/courses/models.py
+++ app/courses/models.py
@@ -1,3 +1,79 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+from app.accounts.models import Teacher, Student
+
+
+class CourseCategory(models.Model):
+    """
+    Course Category - Subject, Seminar, Optional seminar...
+    Used later when configuring couses choosing.
+    """
+
+    class Meta:
+        verbose_name = _("Course category")
+        verbose_name_plural = _("Course categories")
+
+    acronym = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=False, blank=True, default="")
+
+    def __str__(self):
+        return self.name
+
+
+class SubjectClass(models.Model):
+    """
+    Subject class - e.g Math, Biology, ...
+    Every course must have unique Subject Class.
+    """
+
+    class Meta:
+        verbose_name = _("Subject class")
+        verbose_name_plural = _("Subject classes")
+
+    acronym = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100, unique=True)
+    teachers = models.ManyToManyField(to=Teacher, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    """
+    Course for specific grade/year. It's better to distinquish it.
+    Course does not have grade as attribute, becouse some courses can be
+    independed from year, e.g. optional couse: Cisco CCNA.
+
+    Examples:
+      - (03-MATH) Maths for 3th year
+      - (03-SEM) Maths seminar for 3th year
+      - (04-SEM) Maths seminar for 4th year
+    """
+
+    class Meta:
+        ordering = ("-active", "acronym",)
+        unique_together = ("name", "category")
+        verbose_name = _("Course")
+        verbose_name_plural = _("Courses")
+
+    active = models.BooleanField(default=True)
+    category = models.ForeignKey(to=CourseCategory)
+    subject_class = models.ForeignKey(to=SubjectClass)
+    acronym = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    teachers = models.ManyToManyField(to=Teacher, blank=True)
+    info_goal = models.CharField(max_length=512, blank=True, default="")
+    info_content = models.CharField(max_length=512, blank=True, default="")
+    info_anotations = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return self.acronym + ": " + self.name
+
+    def teachers_str(self):
+        return ', '.join(teacher.user.get_full_name() for teacher in self.teachers.all()) or None
+
