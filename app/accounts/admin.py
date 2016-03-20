43d000e40026adf6322c7c8e93d94702f877d34e
Lukáš Kasič <lukas@kasic.sk>
2016-03-20 18:20:23 +0100

    Create Accounts models - basic profiles for Students and Teachers

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from app.accounts.models import Classroom, Student, Teacher
+
+
+class ClassroomAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'grade', 'name')
+	list_filter = ('grade',)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("user", "classroom")
+    list_filter = ("classroom",)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ("get_titled_name", )
+
+
+admin.site.register(Classroom, ClassroomAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,75 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+"""
+Extension of django.contrib.auth.models.User model.
+Specialization of users for studnents and teachers.
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+
+class Classroom(models.Model):
+
+    class Meta:
+        unique_together = ('grade', 'name')
+        ordering = ('grade', 'name')
+
+    grade = models.IntegerField()
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "{0}.{1}".format(self.grade, self.name)
+
+
+class Student(models.Model):
+    """
+    User specialization - Student Profile.
+    """
+
+    class Meta:
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
+        ordering = ('classroom', 'user')
+
+    user = models.OneToOneField(to=User)
+    classroom = models.ForeignKey(to=Classroom)
+
+    def __repr__(self):
+        return "<Student: {0} ({1})>".format(self.user.username, str(self.classroom))
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Teacher(models.Model):
+    """
+    User specialization - Teacher Profile
+    """
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
+
+    user = models.OneToOneField(to=User)
+    titles_before = models.CharField(max_length=50, blank=True, null=False, default="")
+    titles_after = models.CharField(max_length=50, blank=True, null=False, default="")
+    #classroom = models.ForeignKey(to=Classroom, blank=True, null=True, related_name="classmaster")
+
+    def get_titled_name(self):
+        out = ""
+        if self.titles_before:
+            out = self.titles_before + " "
+        out += self.user.get_full_name()
+        if self.titles_after:
+            out += ", " + self.titles_after
+        return out
+    get_titled_name.short_description = _("name")
+
+    def __repr__(self):
+        return "<Teacher: {0}>".format(self.user.username)
+
+    def __str__(self):
+        return self.get_titled_name()
+
