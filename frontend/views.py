5b26d51933de5099da5407b958e40b8963d2c0e9
Lukáš Kasič <lukas@kasic.sk>
2016-04-11 11:19:42 +0200

    Frontend/admin - improvement of choosing overview page

--- frontend/views.py
+++ frontend/views.py
@@ -26,6 +26,9 @@ def courses(request):
     return render(request, "pages/courses.html", context)
 
 
+class A:
+    pass
+
 @login_required
 def overview(request, choosing_id = None):
 
@@ -44,10 +47,34 @@ def overview(request, choosing_id = None):
     if choosing and choosing.phase in (1,2,3):
         students_map = {}
         invalid_students = []
+        data = {}
         for student in Student.objects.filter(classroom__grade=choosing.for_grade):
             chooses = student.choose_set.filter(choosing=choosing).exclude(phase=2)
             if chooses.count() < choosing.courses_min or chooses.count() > choosing.courses_max:
                 invalid_students.append( (student, chooses.count()) )
+            for choose in chooses:
+                course = choose.course
+                req = choose.teacherrequest_set.all()
+                if len(req) == 1:
+                    req = req[0]
+                    teacher = req.teacher
+                    if course.id not in data.keys(): data[course.id] = {}
+                    if teacher.id not in data[course.id].keys(): data[course.id][teacher.id] = []
+                    data[course.id][teacher.id].append(req)
+
+                elif len(req) == 0:
+                    if course.id not in data.keys(): data[course.id] = {}
+                    if -1 not in data[course.id].keys(): data[course.id][-1] = []
+                    a = A()
+                    a.choose = choose
+                    a.teacher = None
+                    data[course.id][-1].append(a)
+
+                else:
+                    raise RuntimeError("Assert - choose has more than one teacher requests!")
+
         context['invalid_students'] = invalid_students
+        context['data'] = data
+
 
     return render_to_response("root/overview.html", context, RequestContext(request))
